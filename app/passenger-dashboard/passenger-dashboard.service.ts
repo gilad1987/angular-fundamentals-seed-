2f3ebb236df41103fda84b63b874298d046a1e9a
Gilad Takoni <gilad1987@gmail.com>
2017-06-25 23:42:49 +0300

    change config fil

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,9 +1,11 @@
-import { Http }         from '@angular/http';
+import { Http, Response, Headers, RequestOptions }         from '@angular/http';
 import { Injectable }   from '@angular/core';
 
 import { Passenger }    from './models/passenger.interface';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 const PASSENGER_API = '/api/passengers';
 
@@ -15,18 +17,30 @@ export class PassengerDashboardService{
     getPassengers(): Observable<Passenger[]>{
         return this.http
             .get( PASSENGER_API )
-            .map( (response: Response) => response.json() );
+            .map( (response: Response) => response.json() )
+            .catch( (error: any) => Observable.throw( error.json() ) );
     }
 
     updatePassengers( passenger: Passenger ): Observable<Passenger>{
+
+        let headers = new Headers({
+           'Content-Type':'application/json'
+        });
+
+        let options = new RequestOptions({
+            headers : headers
+        });
+
         return this.http
-            .put(`${PASSENGER_API}/${passenger.id}`, passenger)
-            .map( (response: Response) => response.json() );
+            .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
+            .map( (response: Response) => response.json() )
+            .catch( (error: any) => Observable.throw( error.json() ) );
     }
 
     removePassengers( passenger: Passenger ): Observable<Passenger>{
         return this.http
             .delete(`${PASSENGER_API}/${passenger.id}`)
-            .map( (response: Response) => response.json() );
+            .map( (response: Response) => response.json() )
+            .catch( (error: any) => Observable.throw( error.json() ) );
     }
 }
